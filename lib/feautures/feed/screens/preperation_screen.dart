8c6bf18bf7619a2c21f394ff8ced8e86aa8bc58e
Beyz61 <6trabzn1@gmail.com>
2025-02-14 12:50:24 +0100

    Add SafeArea

--- lib/feautures/feed/screens/preperation_screen.dart
+++ lib/feautures/feed/screens/preperation_screen.dart
@@ -16,111 +16,113 @@ class PreparationScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 70),
-              const Text(
-                "Zubereitung:",
-                style: TextStyle(
-                  fontSize: 50,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  fontFamily: "SFProDisplay",
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      offset: Offset(3, 3),
-                    ),
-                  ],
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 70),
+                const Text(
+                  "Zubereitung:",
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: "SFProDisplay",
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        offset: Offset(3, 3),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: recipe.preparations.length,
-                itemBuilder: (context, index) {
-                  final preparation = recipe.preparations[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5.0),
-                    child: PreparationContainer(
-                      title: preparation.title,
-                      description: recipe.buildPreparationTexts(preparation),
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 10),
-              if (recipe.tipp != null)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Tipp: ",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            fontWeight: FontWeight.w700,
-                            fontStyle: FontStyle.italic,
-                            fontFamily: "SFProDisplay",
-                            fontSize: 18,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Color.fromARGB(255, 203, 20, 20),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: recipe.preparations.length,
+                  itemBuilder: (context, index) {
+                    final preparation = recipe.preparations[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                      child: PreparationContainer(
+                        title: preparation.title,
+                        description: recipe.buildPreparationTexts(preparation),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 10),
+                if (recipe.tipp != null)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Tipp: ",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.italic,
+                              fontFamily: "SFProDisplay",
+                              fontSize: 18,
+                              decoration: TextDecoration.underline,
+                              decorationColor: Color.fromARGB(255, 203, 20, 20),
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          recipe.tipp!,
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 13, 13, 13),
-                            fontSize: 17,
-                            fontWeight: FontWeight.w600,
-                            fontStyle: FontStyle.italic,
-                            fontFamily: "SFProDisplay",
-                            shadows: [
-                              Shadow(
-                                blurRadius: 0.5,
-                                color: Colors.black,
-                                offset: Offset(0, 1),
-                              ),
-                            ],
+                          const SizedBox(height: 5),
+                          Text(
+                            recipe.tipp!,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 13, 13, 13),
+                              fontSize: 17,
+                              fontWeight: FontWeight.w600,
+                              fontStyle: FontStyle.italic,
+                              fontFamily: "SFProDisplay",
+                              shadows: [
+                                Shadow(
+                                  blurRadius: 0.5,
+                                  color: Colors.black,
+                                  offset: Offset(0, 1),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
+                const SizedBox(height: 20),
+                const Text(
+                  " Guten\nAppetit!",
+                  style: TextStyle(
+                    fontSize: 70,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: "SFProDisplay",
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        offset: Offset(3, 3),
+                      ),
+                    ],
+                  ),
                 ),
-              const SizedBox(height: 20),
-              const Text(
-                " Guten\nAppetit!",
-                style: TextStyle(
-                  fontSize: 70,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  fontFamily: "SFProDisplay",
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      offset: Offset(3, 3),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 50),
-            ],
+                const SizedBox(height: 50),
+              ],
+            ),
           ),
         ),
       ),
