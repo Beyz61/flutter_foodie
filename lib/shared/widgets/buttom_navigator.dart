d0790f0dec6a084f81c46928dfaf0acce207916e
Beyz61 <6trabzn1@gmail.com>
2025-02-17 22:10:12 +0100

    Change BottomNavigator to include AI screen and update navigation labels

--- lib/shared/widgets/buttom_navigator.dart
+++ lib/shared/widgets/buttom_navigator.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foodie_screen/feautures/ai/screens/ai_screen.dart'; // Import AI screen
+import 'package:foodie_screen/feautures/ai/screens/ai_screen.dart';
 import 'package:foodie_screen/feautures/discover/screens/spot_screen.dart';
 import 'package:foodie_screen/feautures/favorite/screens/favorit_screen.dart';
 import 'package:foodie_screen/feautures/feed/screens/feed_screen.dart';
@@ -14,7 +14,7 @@ class ButtonNavigator extends StatefulWidget {
 }
 
 class _ButtonNavigator extends State<ButtonNavigator> {
-  int _selectedPage = 0; 
+  int _selectedPage = 0;  
   late final List<Widget> _screens;
 
   @override
@@ -22,8 +22,8 @@ class _ButtonNavigator extends State<ButtonNavigator> {
     super.initState();
     _screens = [
       const FeedScreen(),
-      const AIScreen(), 
       const FavoritScreen(),
+      const AIScreen(), 
       const SpotScreen(),
       const SettingsScreen(),
     ];
@@ -53,11 +53,11 @@ class _ButtonNavigator extends State<ButtonNavigator> {
         unselectedItemColor: const Color.fromARGB(255, 225, 175, 131),
         currentIndex: _selectedPage,
         onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
+        type: BottomNavigationBarType.fixed, 
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.restaurant), label: "Heute"),
-          BottomNavigationBarItem(icon: Icon(Icons.fastfood_rounded), label: "AI"), 
           BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favoriten"),
+          BottomNavigationBarItem(icon: Icon(Icons.auto_awesome_outlined), label: "AI"), 
           BottomNavigationBarItem(icon: Icon(Icons.public), label: "Entdecken"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
         ],
